db8e2d4e1c6dd85cec4f70ce610e50e537cb975e
Jayesh Ranjan <jranjan@TCICREDIT.LOCAL>
2019-07-03 16:11:28 -0400

    Basic Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
+import { CenterComponent } from './center/center.component';
+import { BottomComponent } from './bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    RightComponent,
+    CenterComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
