6c152e4ea99528e7705e0b2a25569b146a6a8003
OJayeshRanjanO <jayesh.ranjan20@gmail.com>
2019-07-09 17:13:18 -0400

    Plays music

--- src/app/lower/lower.component.ts
+++ src/app/lower/lower.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Howl, Howler } from 'howler';
 
 @Component({
   selector: 'app-lower',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lower.component.scss']
 })
 export class LowerComponent implements OnInit {
-
+  sound = null;
   constructor() { }
 
   ngOnInit() {
+
+  }
+  
+  public playSong() {
+
+    this.sound = new Howl({
+      src: 'http://prclive1.listenon.in:9960/?fbclid=IwAR1bAO9Hf-yvOGrjKVVdYt0XXnqo85o1G2IXWrzVtjIujOit5JqW7oQUtfI',
+      format: ['mp3', 'aac'],
+      autoplay: true,
+      html5: true
+    });
+
+    this.sound.play()
+
+  }
+
+  public pauseSong() {
+    this.sound.pause()
+
+  }
+
+  public getPos() {
+    console.log(this.sound.pos)
   }
 
 }
